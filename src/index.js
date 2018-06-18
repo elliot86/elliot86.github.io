eb1ab45799f33c581355d4e152ab0f955f20d9fa
Elliot Ritchie <eritchie@consoleconnect.com>
2018-06-18 13:04:11 +1000

    Init v2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import _ from 'lodash';
-
-function component() {
-    const element = document.createElement('div');
-
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
-}
-
-document.body.appendChild(component());
+import React from 'react';
+import './styles.css';
+const MyComponent = () => (
+    <h1>Hello from My Component</h1>
+);
+export default MyComponent;
